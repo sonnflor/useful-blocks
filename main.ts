14e23a0ae698bc6e7ba7d6fe452382535a0833a9
sonnflor <106320358+sonnflor@users.noreply.github.com>
2025-09-11 17:43:18 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 //% weight=100 color=#0fbc11 icon="↻"
+//% block="Useful Blocks"
 namespace usefulBlocks {
+    /**
+     * Wiederhole den Block solange, bis die Bedingung erfüllt ist.
+     */
     //% block="repeat until %condition"
     //% condition.shadow=logic_boolean
+    //% group="Control"
+    //% blockId=useful_repeat_until
     export function repeatUntil(condition: () => boolean, body: () => void): void {
         while (!condition()) {
             body();
