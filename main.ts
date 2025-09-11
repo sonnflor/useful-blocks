f307f8a8219274bdaffc1c074dbe88471e094dbe
sonnflor <106320358+sonnflor@users.noreply.github.com>
2025-09-11 17:36:56 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 //% weight=100 color=#0fbc11 icon="↻"
 namespace usefulBlocks {
-    /**
-     * Führt die Schleife aus, solange die Bedingung wahr ist.
-     */
-    //% block="whileLoop %condition=logic_boolean do"
-    export function whileLoop(condition: () => boolean, body: () => void): void {
-        while (condition()) {
+    //% block="repeat until %condition"
+    //% condition.shadow=logic_boolean
+    export function repeatUntil(condition: () => boolean, body: () => void): void {
+        while (!condition()) {
             body();
         }
     }
