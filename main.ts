2737add2f308fa169dfa451e205cce524388b84e
sonnflor <106320358+sonnflor@users.noreply.github.com>
2025-09-11 17:28:15 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 namespace usefulBlocks {
-    //%block
-    export function whileLoop(x:number) {
-        
+    //% block="while $condition do"
+    //% condition.shadow=logic_boolean
+    //% blockType="statement"
+    export function whileLoop(condition: () => boolean, body: () => void): void {
+        while (condition()) {
+            body();
+        }
     }
 }
