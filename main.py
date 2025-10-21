f81345d7b584452e83ed374d77e13e70f5cce1dc
sonnflor <106320358+sonnflor@users.noreply.github.com>
2025-10-21 17:28:21 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+@namespace
+class usefulBlocks:
+    """
+    Wiederhole den Block solange die Bedingung erfüllt ist. 
+    """
+    # % block="macheRot"
+    def setRedLight():
+        basic.set_led_color(0xff0000)
--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
-//% weight=100 color=#0fbc11 icon="↻"
 namespace usefulBlocks {
-    /**
-     * Wiederhole den Block solange die Bedingung erfüllt ist.
-     */
-    //% block="während %condition"
-    //% condition.shadow=logic_boolean
-    export function whileLoop(condition: () => boolean, body: () => void): void {
-        while (condition()) {
-            body();
-        }
+    /** Wiederhole den Block solange die Bedingung erfüllt ist. */
+    //% block="macheRot"
+    function setRedLight() {
+        basic.setLedColor(0xff0000)
     }
+    
 }
+
