c2941e3e75513d11510340ed4454647ebde74d23
sonnflor <106320358+sonnflor@users.noreply.github.com>
2025-10-21 17:33:28 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 namespace usefulBlocks {
     /** Wiederhole den Block solange die Bedingung erfüllt ist. */
     //% block="macheRot"
-    function setRedLight() {
+    function setRedLight():void {
         basic.setLedColor(0xff0000)
     }
     
